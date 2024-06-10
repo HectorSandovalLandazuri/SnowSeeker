f7d2c70e3ace4c10868e41585ac2a8b16f9c2653
Héctor Sandoval <ingsan@live.com.mx>
2024-06-10 11:57:31 -0600

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
